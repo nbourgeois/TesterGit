d5d475cd5bfedd7aa5161b9d14b45be1bb68829b
Nicolas <Nicolas@PortableNb>
2013-09-14 20:35:41 +0200

    Merge TesterGit/master
    Conflicts:
    	src/testergit/TesterGit.java

--- src/testergit/TesterGit.java
+++ src/testergit/TesterGit.java
@@ -17,7 +17,7 @@ public class TesterGit {
     public static void main(String[] args) {
         System.out.println("Version 3.0");
         System.out.println("J'ai fait des modifs");
-        System.out.println("J'ai fait une modif");
+        System.out.println("J'ai fait une modif, je suis pas d'accord");
     }
     
 }
